c1df4e4f49ef5a1ccfdb7cb77c4d3956439fd19d
amine0129 <nemaramine2001@gmail.com>
2023-09-07 17:09:50 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: A C program that prints with put function
@@ -9,7 +8,6 @@
  * Return: Always 0 (Succsess)
 */
 int main(void)
-{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
