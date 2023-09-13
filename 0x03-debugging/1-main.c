66da6c367ee3748af0d6680ab399679632bf6903
amine0129 <nemaramine2001@gmail.com>
2023-09-13 15:11:37 -0700

    a

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("infinitie loop incoming :(\n");
+
+	i = 0;
+
+	/*wihle (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("infinitie loop avoid! \\o/\n");
+
+	return (0);
+}
