193a9f564c24684ddc2dd0cf797595bf905ac803
amine0129 <nemaramine2001@gmail.com>
2023-09-14 10:05:07 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ * 		only using _outchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	DO {
+		_putchar(num + 48);
+		num++;
+	}wihle (num >= 0 && num <= 9);
+	_putchar ('\n');
+}
