dc525c70350e3187804e8c627830e01c023f30d9
amine0129 <nemaramine2001@gmail.com>
2023-10-02 15:54:13 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,41 +1,38 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - adds positive numbers
- * @argv: array of pointers to strings
- * @argc: argument count
- *
- * Return: 0 or 1
- */
-int main(int argc, char *argv[])
-{
-	int array[] = {25, 10, 5, 2, 1};
-	int i, cents;
-	int count = 0;
 
-	if (argc != 2)
+/**
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed
+ * @argv: number of variables to calculate
+ * Return: always 0 for sucess
+ */
+
+int main(int argc, char **argv)
+{
+	int total, change;
+
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
+
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
 	{
-		printf("0\n");
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
 	}
-	for (i = 0; i < 5; i++)
-	{
-		if (cents >= array[i])
-		{
-			count += (cents / array[i]);
-			cents = cents % array[i];
-		}
-		if (cents == 0)
-		{
-			printf("%d\n", count);
-			break;
-		}
-	}
+	printf("%d\n", total);
 	return (0);
 }
